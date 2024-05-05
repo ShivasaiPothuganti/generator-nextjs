3333bf11061de44686a60964a2f08b60b1f7e194
= <=>
2024-05-05 22:36:12 +0530

    intial template

---
+++ generators/app/templates/src/app/components/PrivateRoute.jsx
@@ -0,0 +1,31 @@
+'use client';
+
+
+import { signIn, useSession } from "next-auth/react";
+import React from 'react'
+
+function PrivateRoute({children}) {
+    const authData = useSession({
+        required:true,
+        status:'unauthenticated',
+        onUnauthenticated:()=>{
+            signIn();
+        }
+    });
+    const {status} = authData;
+    const isAuthenticated = status === 'authenticated';
+
+    if(status==='loading'||status==='unauthenticated'){
+        return <h1>Redirecting you to the page</h1>
+    }
+
+  return (
+    <>
+    
+        {children}
+   
+    </>
+  )
+}
+
+export default PrivateRoute;
